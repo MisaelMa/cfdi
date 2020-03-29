c430411ea2a0cc54927d4e79c731fd06597c1e58
MisaelMa <amisael.amir.misael@gmail.com>
2020-03-28 21:26:23 -0500

    se agrego la exportacion de las clases

--- src/signati/complements/pago10/index.ts
+++ src/signati/complements/pago10/index.ts
@@ -1,4 +1,8 @@
-import {Pago10} from './Pago10';
+import { Pago10 } from './Pago10';
+import { Pago10Relacionado } from './Pago10Relacionado';
+import { Pago10Impuestos } from './Pago10Impuestos';
 
+export { Pago10Relacionado };
+export { Pago10Impuestos };
 export { Pago10 };
 export default Pago10;
