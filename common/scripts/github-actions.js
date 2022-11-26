cd5035290782a5309a23f279d6d750c9db5f81af
MisaelMa <amisael.amir.misael@gmail.com>
2022-11-26 13:25:01 -0500

    feat(xml): TEST

--- common/scripts/github-actions.js
+++ common/scripts/github-actions.js
@@ -33,15 +33,13 @@ module.exports = async ({github, context, core}) => {
 
   for (var i = 0; i < commits.length; i++) {
     const commit = commits[i];
-    console.log("commit",commit)
-  }
-  for (const commit of commits) {
-    const commit = commit.message;
-    const [type, msg] = commit.split(':')
+    const message = commit.message;
+    const [type, msg] = message.split(':')
     const scope = type.match(/\(([^)]+)\)/g).pop().replace(/[{()}]/g, '')
     if (list.includes(scope)){
       const data = await execa('rush', ['version', '--version-policy', scope, '--bump' ])
       console.log(scope, stdout);
     }
+    console.log("commit",commit)
   }
 }
