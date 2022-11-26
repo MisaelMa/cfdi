42029b3ecb08c41ca1034f58356df77a82396423
MisaelMa <amisael.amir.misael@gmail.com>
2022-11-26 13:16:07 -0500

    feat(xml): data: dad

--- common/scripts/github-actions.js
+++ common/scripts/github-actions.js
@@ -26,7 +26,9 @@ async function execa(command, params) {
 
 
 module.exports = async ({github, context, core}) => {
-  const commits = github.event.commits
+  console.log(JSON.stringify(github))
+  console.log(JSON.stringify(context))
+  const commits = github.event.commits || []
   // const list = ['catalogs','csd','curp','pdf','rfc','utils','xml','openssl','saxon']
   const list = ['catalogs','csd','utils','xml','openssl','saxon']
   for (const commit of commits) {
