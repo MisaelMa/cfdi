b4b13e31f5932090c31cc3f1d6ff9ffc33ec788a
MisaelMa <amisael.amir.misael@gmail.com>
2022-11-26 13:01:00 -0500

    feat(xml): data: dad

--- common/scripts/github-actions.js
+++ common/scripts/github-actions.js
@@ -1,6 +1,31 @@
 
+async function execa(command, params) {
+  const { spawn } = require('child_process');
+  const child = spawn(command, params);
 
-module.exports = async ({github, context, core, execa}) => {
+  let data = "";
+  for await (const chunk of child.stdout) {
+      console.log('stdout chunk: '+chunk);
+      data += chunk;
+  }
+  let error = "";
+  for await (const chunk of child.stderr) {
+      console.error('stderr chunk: '+chunk);
+      error += chunk;
+  }
+  const exitCode = await new Promise( (resolve, reject) => {
+      child.on('close', resolve);
+  });
+
+  if( exitCode) {
+      throw new Error( `subprocess error exit ${exitCode}, ${error}`);
+  }
+  return data;
+}
+
+
+
+module.exports = async ({github, context, core}) => {
   const commits = github.event.commits
   // const list = ['catalogs','csd','curp','pdf','rfc','utils','xml','openssl','saxon']
   const list = ['catalogs','csd','utils','xml','openssl','saxon']
@@ -9,7 +34,7 @@ module.exports = async ({github, context, core, execa}) => {
     const [type, msg] = commit.split(':')
     const scope = type.match(/\(([^)]+)\)/g).pop().replace(/[{()}]/g, '')
     if (list.includes(scope)){
-      const { stdout } = await execa('rush', ['version', '--version-policy', scope, '--bump' ])
+      const data = await execa('rush', ['version', '--version-policy', scope, '--bump' ])
       console.log(scope, stdout);
     }
   }
