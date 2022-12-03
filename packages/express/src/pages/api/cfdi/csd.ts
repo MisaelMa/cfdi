26303a646f5e81d82a3fc2638e6a4bb6a64a9732
MisaelMa <amisael.amir.misael@gmail.com>
2022-12-03 15:46:20 -0500

    feat(core): config

--- packages/express/src/pages/api/cfdi/csd.ts
+++ packages/express/src/pages/api/cfdi/csd.ts
@@ -7,6 +7,8 @@ export default async function loginRoute(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const cadena =
+    '||4.0|E|ACACUN-27|2014-07-08T12:16:50|01|20001000000300022815|16148.04|645.92|MXN|17207.35|I|01|PUE|México|1|1|2|01|asdasd-3234-asdasd-2332-asdas|asdasd-3234-asdasd-2332-asdas|TCM970625MB1|FACTURACION MODERNA SA DE CV|601|asdasd|XAXX010101000|PUBLICO EN GENERAL|112|22|G01|001|1212|2|pieza|Pieza|audifonos|1000|2000|00.0|01|369.83|002|Tasa|0.16|59.17|369.8aaaa3|002|Tasa|0.16|59.17|369.83|002|Tasa|0.16|59.17|21 47 3807 8003832|000121231|51241200|IM020|1|PIEZA|25311FM00114 CREMA FUNGICIDA 35ML (ACIDO UNDECILENICO, ARBOL DEL TE VEHICULO EMOLIENTE)|172.50|172.50|001|1212|2|pieza|Pieza|audifonos|1000|2000|00.0|01|JUFA7608212V6|ADRIANA JUAREZ FERNANDEZ|601|29133|002|59.17|1000|1|002|Tasa|0.16|59.17||';
   const fil = path.join(
     __dirname,
     '..',
@@ -44,7 +46,7 @@ export default async function loginRoute(
       serial: cer.serial(),
       cer: cer.getNoCer(),
       pubkey: cer.pubkey({ begin: true }),
-      module: cer.modulu(),
+      //module: cer.modulu(),
       // subjectHash: cer.subjectHash(),
       // issuerHash: cer.issuerHash(),
       // ocspid: cer.ocspid(),
