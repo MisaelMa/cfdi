b9cd4914419a813eb297045dc64169f02f3ab144
MisaelMa <amisael.amir.misael@gmail.com>
2023-04-29 20:27:38 -0500

    feat(config): config

--- packages/express/next.config.js
+++ packages/express/next.config.js
@@ -17,6 +17,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: isProdMode,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
