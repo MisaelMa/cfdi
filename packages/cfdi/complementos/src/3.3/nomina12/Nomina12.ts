90d48fe716429d8e1f09bdf5fdaef42e5fca1c0d
MisaelMa <amisael.amir.misael@gmail.com>
2023-05-08 12:39:12 -0500

    feat(only-complementos): se libera el complemento ine

--- packages/cfdi/complementos/src/3.3/nomina12/Nomina12.ts
+++ packages/cfdi/complementos/src/3.3/nomina12/Nomina12.ts
@@ -1,27 +1,21 @@
-import { XmlIne, XmlIneAttribute } from '../../types';
-
 /**
  *
  */
 export default class Nomina12 {
-  private nomina: XmlIne = {} as XmlIne;
-
+  //private nomina: XmlIne = {} as XmlIne;
   // private xmlns = 'http://www.sat.gob.mx/nomina12';
-
   // private xmlnskey = 'nomina12';
-
   // private schemaLocation: string[] = [
   //   'http://www.sat.gob.mx/nomina12',
   //   'http://www.sat.gob.mx/nomina12 http://www.sat.gob.mx/sitio_internet/cfd/nomina/nomina12.xsd',
   // ];
-
   /**
    *constructor
    *
    * @param attributes
    * XmlIneAttribute
    */
-  constructor(attributes: XmlIneAttribute) {
+  /* constructor(attributes: XmlIneAttribute) {
     this.nomina._attributes = attributes;
-  }
+  } */
 }
