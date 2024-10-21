8d8faf6f888166031903fc6c9f6ade11e5b69bae
MisaelMa <amisael.amir.misael@gmail.com>
2024-10-21 10:13:22 -0500

    feat(xml): remove loh

--- packages/cfdi/xml/vitest.config.ts
+++ packages/cfdi/xml/vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { resolve } from 'path';
-console.log(resolve(__dirname, '../xsd/src'), 'resolve amir');
 export default defineConfig({
   test: {
     // Aquí puedes incluir configuraciones específicas para Vitest
